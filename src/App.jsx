ab31d119dd3da78b79408162c9571119f2f26fb8
PaulCrtr <paul.th.cartier@gmail.com>
2020-07-03 18:28:06 +0200

    main components added to the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import List from "./components/List";
+import FullLyrics from "./components/FullLyrics";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/list" exact component={List} />
+        <Route path="/lyrics" exact component={FullLyrics} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/FullLyrics.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FullLyrics = () => {
+  return <div></div>;
+};
+
+export default FullLyrics;
