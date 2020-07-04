b9414f0c2b540bbff7d5ed7aa855c7390df43b4b
PaulCrtr <paul.th.cartier@gmail.com>
2020-07-04 19:52:17 +0200

    Songs grouped by artist on list page

--- src/components/FullLyrics.jsx
+++ src/components/FullLyrics.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const FullLyrics = (props) => {
-  const { item } = props.location.state;
+  const { datas } = props.location.state;
 
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/list">My list</Link>
-      <h2>{item.artist}</h2>
-      <h2>{item.title}</h2>
+      <h2>{datas.artist}</h2>
+      <h2>{datas.title}</h2>
       <iframe
-        title={item.title}
+        title={datas.title}
         scrolling="no"
-        frameborder="0"
-        allowTransparency="true"
-        src={`https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=${item.id}&app_id=1`}
+        frameBorder="0"
+        allowtransparency="true"
+        src={`https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=${datas.id}&app_id=1`}
         width="700"
         height="100"
       />
-      <p>{item.lyrics}</p>
+      <p>{datas.lyrics}</p>
     </div>
   );
 };
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const List = () => {
-  const list = JSON.parse(window.localStorage.getItem("myLyrics"));
+  // Songs grouped by artists
+  const list = JSON.parse(window.localStorage.getItem("myLyrics")).reduce(
+    (element, { artist, id, title, picture, lyrics }) => {
+      (element[artist] = element[artist] || []).push({
+        id,
+        title,
+        picture,
+        lyrics,
+      });
+      return element;
+    },
+    {}
+  );
 
   return (
     <div>
       <Link to="/">Home</Link>
-      {list.map((item) => (
-        <div key={item.id}>
-          <Link to={{ pathname: "/lyrics", state: { item } }}>
-            <h2>{item.artist}</h2>
-            <h3>{item.title}</h3>
-            <img src={item.picture} alt={item.artist} />
-          </Link>
+      {Object.keys(list).map((key, i) => (
+        <div key={i}>
+          <h2>{key}</h2>
+          {list[key].map((datas) => (
+            <Link to={{ pathname: "/lyrics", state: { datas } }} key={datas.id}>
+              <h3>{datas.title}</h3>
+              <img src={datas.picture} alt={datas.artist} />
+            </Link>
+          ))}
         </div>
       ))}
     </div>
