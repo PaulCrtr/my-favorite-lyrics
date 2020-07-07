949f36dc24f0ceb77c6d0217eaee4cae1ab55c69
PaulCrtr <paul.th.cartier@gmail.com>
2020-07-07 22:13:37 +0200

    added snapshot tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,56 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { shallow } from "enzyme";
+import App from "./App";
 import Home from "./components/Home";
+import List from "./components/List";
+import FullLyrics from "./components/FullLyrics";
 import RequestForm from "./components/RequestForm";
 
-test("renders List link", () => {
-  const { getByText } = render(
-    <Router>
-      <Home />
-    </Router>
-  );
-  const listLink = getByText("My list");
-  expect(listLink).toBeInTheDocument();
-  expect(<RequestForm />).toBeInTheDocument();
+const testItem1 = {
+  id: 10,
+  artist: "test 1",
+  title: "test 1",
+  picture: "test 1",
+  lyrics: "This is a test 1.",
+};
+
+const testItem2 = {
+  id: 20,
+  artist: "test 2",
+  title: "test 2",
+  picture: "test 2",
+  lyrics: "This is a test 2.",
+};
+
+describe("App", () => {
+  beforeAll(() => {
+    localStorage.myLyrics = JSON.stringify([testItem1, testItem2]);
+  });
+
+  it("renders App correctly", () => {
+    const appWrapper = shallow(<App />);
+    expect(appWrapper).toMatchSnapshot();
+  });
+
+  it("renders Home correctly", () => {
+    const homeWrapper = shallow(<Home />);
+    expect(homeWrapper).toMatchSnapshot();
+  });
+
+  it("renders RequestForm correctly", () => {
+    const requestFormWrapper = shallow(<RequestForm />);
+    expect(requestFormWrapper).toMatchSnapshot();
+  });
+
+  it("renders List correctly", () => {
+    const listWrapper = shallow(<List />);
+    expect(listWrapper).toMatchSnapshot();
+  });
+
+  it("renders Lyrics correctly", () => {
+    const fullLyricsWrapper = shallow(
+      <FullLyrics location={{ state: { datas: testItem1 } }} />
+    );
+    expect(fullLyricsWrapper).toMatchSnapshot();
+  });
 });
